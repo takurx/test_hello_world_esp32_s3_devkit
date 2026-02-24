854d59f9058438a42557e3e46ae17fba1043aba3
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2026-02-24 16:13:56 +0900

    add comment
    add comment

--- test_esp32s3_led_chika/src/main.cpp
+++ test_esp32s3_led_chika/src/main.cpp
@@ -18,12 +18,15 @@ void loop() {
   neopixelWrite(LED_PIN, 0, 0, 0); // Off
   delay(1000);
   Serial.println("red");
+  //neopixelWrite(LED_PIN, 255, 0, 0); // Red
   neopixelWrite(LED_PIN, 127, 0, 0); // Red
   delay(1000);
   Serial.println("green");
+  //neopixelWrite(LED_PIN, 0, 255, 0); // Green
   neopixelWrite(LED_PIN, 0, 127, 0); // Green
   delay(1000);
   Serial.println("blue");
+  //neopixelWrite(LED_PIN, 0, 0, 255); // Blue
   neopixelWrite(LED_PIN, 0, 0, 127); // Blue
   delay(1000);
 }
